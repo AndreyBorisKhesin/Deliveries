65c2fc7403a66ae9851acd36e8b021a1584cce71
christinekc <christinekc.cheng@gmail.com>
2018-02-03 18:02:06 -0500

    scheduler

--- profit.py
+++ profit.py
@@ -2,29 +2,29 @@ from numpy import *
 
 past = loadtxt("monthly_sales.csv")
 shop_coords = {
-	 0: ( 2, 6)
-	 1: (10, 6)
-	 2: ( 3, 5)
-	 3: ( 5, 5)
-	 4: ( 8, 5)
-	 5: ( 0, 4)
-	 6: ( 2, 3)
-	 7: ( 6, 3)
-	 8: (10, 3)
-	 9: (12, 3)
-	10: ( 1, 2)
-	11: ( 4, 2)
-	12: (11, 2)
-	13: ( 3, 1)
-	14: ( 8, 1)
-	15: (10, 1)
-	16: ( 2, 0)
-	17: ( 4, 0)
-	18: ( 7, 0)
-	19: (12, 0)
-	20: ( 4, 3)
-	21: ( 9, 2)
-	22: ( 1, 4)
+	 0: ( 2, 6),
+	 1: (10, 6),
+	 2: ( 3, 5),
+	 3: ( 5, 5),
+	 4: ( 8, 5),
+	 5: ( 0, 4),
+	 6: ( 2, 3),
+	 7: ( 6, 3),
+	 8: (10, 3),
+	 9: (12, 3),
+	10: ( 1, 2),
+	11: ( 4, 2),
+	12: (11, 2),
+	13: ( 3, 1),
+	14: ( 8, 1),
+	15: (10, 1),
+	16: ( 2, 0),
+	17: ( 4, 0),
+	18: ( 7, 0),
+	19: (12, 0),
+	20: ( 4, 3),
+	21: ( 9, 2),
+	22: ( 1, 4),
 	23: ( 6, 1)
 }
 
@@ -39,6 +39,47 @@ def customers():
 	return [[[int(round(past[month, 2 * store + product] *
 		(0.9 + 0.2 * random.rand()))) for product in range(2)]
 		for store in range(20)] for month in range(12)]
+
+def schedule():
+	schedule = []
+
+	for day in range(1):
+		warehouse_capacity = [[560, 560], [560, 560]]
+		for w in range(2):
+			while warehouse_capacity[w][0] > 0 or warehouse_capacity[w][1] > 0:
+				p1, p2 = 0, 0
+				if warehouse_capacity[w][0] > 0:
+					p1 = 20 / 0.4
+				if warehouse_capacity[w][1] > 0:
+					p2 = (20 - 0.4 * p1) // 0.8
+				route = {"date": day, 
+						"stops": [[w+20, p1, p2], [w+22, -p1, -p2], [w+20, 0, 0]]}
+				schedule.append(route)
+
+		# fill up stores (0 - 19)
+		shop_capacity = [[15, 15, 20, 20, 15, 20, 30, 30, 35, 25, 
+						30, 30, 30, 35, 30, 40, 25, 20, 15, 20],
+						[15, 15, 20, 20, 15, 20, 30, 30, 35, 25, 
+						30, 30, 30, 35, 30, 40, 25, 20, 15, 20]]
+	
+		for store in range(len(20)):
+			if store in [0, 2, 3, 5, 6, 10, 11, 13, 16, 17]:
+				warehouse = 20
+			else:
+				warehouse = 21
+			while shop_capacity[0][store] > 0 or shop_capacity[1][store] > 0:
+				p1, p2 = 0, 0
+				if shop_capacity[0][store] > 0:
+					p1 = shop_capacity[0][store]
+					shop_capacity[0][store] = 0
+				if shop_capacity[1][store] > 0:
+					p2 = (20 - 0.4 * p1) // 0.8
+					shop_capacity[1][store] -= p2
+				route = {"date": day, 
+						"stops": [[warehouse, p1, p2], [store, -p1, -p2], [warehouse, 0, 0]]}
+				schedule.append(route)
+
+	return schedule
 
 def profit(schedule):
 	"""
@@ -69,8 +110,6 @@ def profit(schedule):
 		for day in range(len(313)):
 
 			for j in range(route_idx, len(schedule)):
-
-				truck_capacity = 
 
 				if schedule[j]["day"] == day:
 					route = schedule[j]
