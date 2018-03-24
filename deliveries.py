afa0af59d88e06c9f24357401762c73625089a87
Andrey Boris Khesin <andrey.khesin@gmail.com>
2018-03-24 18:29:37 -0400

    added extra truck handling

--- deliveries.py
+++ deliveries.py
@@ -1,6 +1,8 @@
 from numpy import *
 
 past = loadtxt("monthly_sales.csv").reshape((12, 20, 2))
+capacity = [15, 15, 20, 20, 15, 20, 30, 30, 35, 25, 30, 30, 30, 35, 30, 40, 25, 20, 15,
+	20, 650, 650, 1000000000, 1000000000]
 shop_coords = {
 	 0: ( 2, 6),
 	 1: (10, 6),
@@ -50,6 +52,58 @@ def generate_customers():
 					customers[int(sums[i] + days[i] *
 						random.rand()), j, k] += 1
 	return customers.astype(int)
+
+def one_day_schedule():
+	schedule = []
+
+	for day in range(1):
+		warehouse_capacity = [[650, 650], [650, 650]]
+		for w in range(2):
+			while max(warehouse_capacity[w]) > 0:
+				p1, p2 = 0, 0
+				if warehouse_capacity[w][0] > 0:
+					p1 = 25
+					warehouse_capacity[w][0] -= p1
+				if warehouse_capacity[w][1] > 0:
+					p2 = int((20 - 0.8 * p1) / 0.4)
+					warehouse_capacity[w][1] -= p2
+				route = {
+					"date": day,
+					"stops": asarray([[w + 20, 0, 0],
+						[w + 22, p1, p2],
+						[w + 20, - p1, - p2]])
+				}
+				schedule = append(schedule, [route])
+
+		# fill up stores (0 - 19)
+		shop_capacity = [[15, 15, 20, 20, 15, 20, 30, 30, 35, 25, 30, 30, 30, 35,
+			30, 40, 25, 20, 15, 20], [15, 15, 20, 20, 15, 20, 30, 30, 35, 25,
+			30, 30, 30, 35, 30, 40, 25, 20, 15, 20]]
+	
+		for store in range(20):
+			if store in [0, 2, 3, 5, 6, 10, 11, 13, 16, 17]:
+				warehouse = 20
+			else:
+				warehouse = 21
+			while max(shop_capacity[0][store],
+				shop_capacity[1][store]) > 0:
+				p1, p2 = 0, 0
+				if shop_capacity[1][store] > 0:
+					p2 = shop_capacity[1][store]
+					shop_capacity[1][store] = 0
+				if shop_capacity[0][store] > 0:
+					p1 = int((20 - 0.4 * p2) / 0.8)
+					shop_capacity[0][store] -= p1
+				route = {
+					"date": day,
+					"stops": asarray([[warehouse, p1, p2],
+						[store, -p1, -p2],
+						[warehouse, 0, 0]])
+				}
+				schedule = append(schedule, [route])
+
+	return schedule
+
 
 def worst_schedule():
 	schedule = []
@@ -102,7 +156,40 @@ def worst_schedule():
 
 	return schedule
 
-def profit(schedule):
+def execute_route(route, stock):
+	truck = [0, 0]
+
+	for stop in route:
+		old_truck = copy(truck)
+
+		for k in range(2):
+			stop[k + 1] = max(min(stop[k + 1],
+				stock[stop[0], k]), - truck[k])
+			stock[stop[0], k] = min(max(
+				stock[stop[0], k] - stop[k + 1],
+				0), capacity[stop[0]])
+
+			# load / unload truck
+			truck[k] = max(stop[k], 0)
+			old_truck[k] += min(stop[k], 0)
+
+		# throw away extra
+		if 2 * truck[0] + truck[1] > 50:
+			if truck[0] <= 25:
+				truck[1] = (50 - 2 * truck[0])
+			else:
+				truck = [25, 0]
+		else:
+			truck[0] += min(old_truck[0], (50 - 2 *
+				truck[0] - truck[1]) // 2)
+			truck[1] += min(old_truck[1], 50 - 2 *
+				truck[0] - truck[1])
+	return stock
+
+def month_from_day(day):
+	return searchsorted(sums, day + 1) - 1
+
+def profit(schedule, extra_route_function):
 	"""
 	schedule: A list of routes
 	route: A dictionary that represents a delivery route of a truck
@@ -129,65 +216,49 @@ def profit(schedule):
 			[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0],
 			[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0],
 			[1000000000, 1000000000], [1000000000, 1000000000]])
+		extra_customers = zeros((20, 2))
 		# s1 - s20, w1, w2, p1, p2
-		capacity = [15, 15, 20, 20, 15, 20, 30, 30, 35, 25, 30, 30, 30, 35, 30,
-			40, 25, 20, 15, 20, 650, 650, 1000000000, 1000000000]
 
 		for day in range(313):
 			routes = []
 
 			for j in range(len(schedule)):
 				if schedule[j]["date"] == day:
-					routes = (append(routes, [schedule[j]["stops"]],
-						axis = 0) if not routes == [] else
-						[schedule[j]["stops"]])
+					routes = ([schedule[j]["stops"]] if routes == []
+						else append(routes,
+						[schedule[j]["stops"]], axis = 0))
 			for route in routes:
 				cost += total_dist(route[:, 0])
-				
-				truck = [0, 0]
+				stock = execute_route(route, stock)
+			
+			month = month_from_day(day)
+			added_routes = extra_route_function(month, day, sum(customers[
+				sums[month]:sums[month + 1]], axis = 0), stock)
 
-				for stop in route:
-					old_truck = copy(truck)
-
-					for k in range(2):
-						stop[k + 1] = max(min(stop[k + 1],
-							stock[stop[0], k]), - truck[k])
-						stock[stop[0], k] = min(max(
-							stock[stop[0], k] - stop[k + 1],
-							0), capacity[stop[0]])
-
-						# load / unload truck
-						truck[k] = max(stop[k], 0)
-						old_truck[k] += min(stop[k], 0)
-
-					# throw away extra
-					if 2 * truck[0] + truck[1] > 50:
-						if 2 * truck[0] <= 50:
-							truck[1] = (50 - 2 * truck[0])
-						else:
-							truck = [25, 0]
-					else:
-						truck[0] += min(old_truck[0], (50 - 2 *
-							truck[0] - truck[1]) // 2)
-						truck[1] += min(old_truck[1], 50 - 2 *
-							truck[0] - truck[1])
+			for added_route in added_routes:
+				cost += 1.2 * total_dist(added_route[:, 0])
+				stock = execute_route(added_route, stock)
 
 			for j in range(20):
 				for k in range(2):
+					total_customers = int(customers[day, j, k] +
+						extra_customers[j, k])
+					extra_customers[j, k] = 0
 					old_stock = stock[j, k]
 					stock[j, k] = max(stock[j, k] -
-						customers[day, j, k], 0)
+						total_customers, 0)
 					revenue += ((300 - 200 * k) *
 						(old_stock - stock[j, k]))
-					for l in range(customers[day, j, k] - old_stock):
-						if (random.rand() < 0.8 and day != 312):
-							customers[day + 1, j, k] += 1
+					for l in range(total_customers - old_stock):
+						if random.rand() < 0.8:
+							extra_customers[j, k] += 1
 
 		profit = revenue - cost
 		profits.append((revenue, cost))
 
 	return profits
 
-#c = generate_customers()
-#print([[[c[i,j,k] for k in range(2)] for j in range(20)] for i in range(313)])
-print(profit(worst_schedule()))
+def a():
+	return 1
+
+print(profit(worst_schedule(), a))
